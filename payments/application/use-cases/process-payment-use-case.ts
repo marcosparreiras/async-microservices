e2a75dc5f37d0952cc7427930e0187259f377260
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-03 00:02:39 -0300

    chore: add order id log on process payment use case

--- payments/application/use-cases/process-payment-use-case.ts
+++ payments/application/use-cases/process-payment-use-case.ts
@@ -10,7 +10,9 @@ export class ProcessPaymentUseCase {
   constructor(private queue: Queue) {}
 
   async execute({ price, creditCardToken, orderId }: Input): Promise<void> {
-    console.log(`Payment approved (${creditCardToken} = ${price} R$)`);
+    console.log(
+      `Payment approved (${creditCardToken} - ${price} R$ - order: ${orderId})`
+    );
     await this.queue.publish("payment-processed", {
       orderId,
     });
